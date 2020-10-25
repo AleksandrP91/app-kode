e24c4c3438a8a4ba292b77cb6b8a602df3e6afd2
Александр Пирогов <aleksandrpirogov@Air-Aleksandr.Dlink>
2020-10-25 20:54:56 +0200

    first commit

---
+++ app-kode/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello, my name is Alex.</p>
+      <p>Hi, gues!</p>
+    </div>
+  );
+}
+
+export default App;
