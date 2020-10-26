5ab137ed67354c2c2a91888b7bf0e481bd97df3d
AleksandrP91 <aleksandrpirogov67@gmail.com>
2020-10-26 08:52:16 +0200

    test

--- app-kode/src/App.js
+++ app-kode/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>Hello, my name is Alex.</p>
-      <p>Hi, gues!</p>
+      <p>test, gitHub</p>
+      <h1>Good morning</h1>
+      <h2>=)</h2>
     </div>
   );
 }
 
-export default App;
+export default App; 
